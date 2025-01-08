b292a815e7666448db60352665a76ce036a756bd
Ben Wernicke <ben@wernicke.at>
2025-01-08 17:21:44 +0100

    what changed?

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    zero2prod_book::run()?.await
+    zero2prod_book::run(
+        TcpListener::bind("localhost:8080").expect("Could not bind TcpListener to port"),
+    )?
+    .await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,13 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
+    let address = format!("{address}/health_check");
 
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(&address)
         .send()
         .await
         .expect("Failed to execute request");
@@ -12,7 +15,11 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod_book::run().expect("Could not create server");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("localhost:0").expect("could not bind tcp listener");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod_book::run(listener).expect("Could not create server");
     tokio::spawn(server);
+
+    format!("http://localhost:{port}")
 }
